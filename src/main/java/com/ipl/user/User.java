7844e43afc074a35e09cadf9861abee1a8daf544
athulraimon <athulraimon@gmail.com>
2024-04-21 21:42:27 +0530

    bruh

--- src/main/java/com/ipl/user/User.java
+++ src/main/java/com/ipl/user/User.java
@@ -17,6 +17,7 @@ public class User {
     @Column(length=45,nullable=false,name="last_name")
     private String lastName;
 
+    private boolean enabled;
     public Integer getId() {
         return id;
     }
@@ -55,5 +56,13 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
     }
 }
--- src/main/java/com/ipl/user/UserController.java
+++ src/main/java/com/ipl/user/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -17,5 +19,17 @@ public class UserController {
         model.addAttribute("listUsers", listUsers);
 
         return "users" ;
+    }
+
+    @GetMapping("/users/new")
+    public String showNewForm(Model model) {
+        model.addAttribute("user", new User());
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user) {
+        service.save(user);
+        return "redirect:/users";
     }
 }
